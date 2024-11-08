5cc34bb1b7159617cda04c4b92f7438f6f8a5188
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-11-08 01:02:57 +0100

    re-setup admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,11 @@
-import NavBar from "./components/NavBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddItem from "./pages/AddItem";
-import Orders from "./pages/Orders";
-import ListItem from "./pages/ListItem";
-import SideBar from "./components/SideBar";
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <SideBar />
-      <Routes>
-        <Route path="/add" element={<AddItem />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/list" element={<ListItem />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const [count, setCount] = useState(0);
+
+  return <></>;
 }
 
 export default App;
